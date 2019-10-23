ac49f0d94bde9c69bfe3248585c570b0cf4a033f
Artūrs Langenfelds <arturs.langenfelds@abc>
2019-10-23 11:13:05 +0300

    added reduce playgroud :)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,110 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
 
-  const [counter, setCounter] = useState(0);
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
 
-  return (
-    <div className="App" data-testid="component-app">
-      <h1>TDD DEMO - CLICK COUNTER APP</h1>
-    </div>
-  );
+  render() {
+
+
+    const arr = [1, 2, 3, 4, 5];
+    const sum = arr.reduce((total, current) => {
+      // console.log('total', total);
+      // console.log('current', current);
+      return total + current;
+    }, 5);
+
+    const doubled = arr.reduce((total, current) => {
+      total.push(current * 2);
+      return total;
+    }, []);
+
+    const fruitBasket = [
+      "banana",
+      "cherry",
+      "orange",
+      "apple",
+      "cherry",
+      "orange",
+      "apple",
+      "banana",
+      "cherry",
+      "orange",
+      "fig"
+    ];
+
+    const fruitCatalog = fruitBasket.reduce((catalog, fruit) => {
+      catalog[fruit] = (catalog[fruit] || 0) + 1;
+      return catalog;
+    }, {});
+
+    const data = [
+      { a: "happy", b: "robin", c: ["blue", "green"] },
+      { a: "tired", b: "panther", c: ["green", "black", "orange", "blue"] },
+      { a: "sad", b: "goldfish", c: ["green", "red"] }
+    ];
+
+    const colors = data.reduce((total, currentObj) => {
+      return total.concat(currentObj.c);
+    }, []);
+
+    const uniqueColors = data.reduce((total, currentObj) => {
+      currentObj.c.forEach(color => {
+        if (total.indexOf(color) === -1) {
+          total.push(color);
+        }
+      });
+      return total;
+    }, []);
+
+    function incrementByOne(input) {
+      return input + 1;
+    }
+    function decrement(input) {
+      return input - 1;
+    }
+    function double(input) {
+      return input * 2;
+    }
+
+    const pipeline = [incrementByOne, double, decrement];
+
+    const result = pipeline.reduce((total, func) => {
+      return func(total);
+    }, 1);
+
+    console.log("sum", sum);
+    console.log("doubled", doubled);
+    console.log("fruitCatalog", fruitCatalog);
+    console.log("colors", colors);
+    console.log("uniqueColors", uniqueColors);
+    console.log("function pipeline result", result);
+
+    const { counter } = this.state;
+
+    return (
+      <div className="App" data-testid="component-app">
+        {/* <h1>TDD DEMO - CLICK COUNTER APP</h1> */}
+        <h1 data-testid="counter-display">
+          The counter is currently {counter}
+        </h1>
+        <button data-testid="increment-button" onClick={this.increment}>
+          Increment
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,28 +6,62 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders without crashing", () => {
-  const wrapper = shallow(<App />);
-  // console.log(wrapper.debug())
-  expect(wrapper).toBeTruthy();
-  const appComponent = wrapper.find("[data-testid='component-app']");
-  expect(appComponent.length).toBe(1);
+/**
+ * Factory function to create ShallowWrapper for the App component.
+ * @function setup
+ * @param {object} props - Component props specific to this app.
+ * @param {object} state = Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
 
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+/**
+ * Return ShallowWrapper containing node(s) with given data-testid value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {string} attr  - Value of data-testid attribute for search.
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, attr) => {
+  return wrapper.find(`[data-testid='${attr}']`);
+};
+
+test("renders without crashing", () => {
+  // const wrapper = shallow(<App />);
+  // console.log(wrapper.debug())
+  const wrapper = setup();
+
+  expect(wrapper).toBeTruthy();
+  // const appComponent = wrapper.find("[data-testid='component-app']");
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
 });
 
 test("renders increment button", () => {
-
+  const wrapper = setup();
+  const buttonComponent = findByTestAttr(wrapper, "increment-button");
+  expect(buttonComponent.length).toBe(1);
 });
 
 test("renders counter display", () => {
-  
-})
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
 
 test("counter starts at 0", () => {
-  
-})
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
 
 test("clicking button increments counter display", () => {
-  
-})
-
+  const wrapper = setup(null, { counter: 7 });
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate('click');
+  expect(wrapper.state("counter")).toBe(8);
+});
